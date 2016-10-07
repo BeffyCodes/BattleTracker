58be2ff77f77637f05f88d8ab444f78f476ef0e4
Jesse Ward <jward1990@gmail.com>
2016-10-07 17:52:12 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,3 @@
+/// <reference path="../app.d.ts" />
+console.log(angular);
+console.log('\'Allo \'Allo!');
---
+++ typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/angular-material/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/typescript/index.d.ts" />
